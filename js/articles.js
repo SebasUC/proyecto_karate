a3d2efaf28c45b7ed4de65d41a7bd0f13f905ca5
LuisDa7 <lviquez164@gmail.com>
2024-06-25 11:09:48 -0600

    avance de blog

---
+++ js/articles.js
@@ -0,0 +1,98 @@
+const articles = [
+    {
+        id: 1,
+        title: "Competencias recientes",
+        thumbnail: "./assets/blog/posts/1.png",
+        content: [
+            {
+                title: "Clasificados a JDN",
+                image: "./assets/blog/articles/article1/1.png",
+                description: [
+                    {
+                        subtitle: "Eliminatoria Juegos Nacionales",
+                        paragraph: "Sigue creciendo nuestra representación de Karate y tenemos más clasificados en sus diferentes categorías: Sub 21, Kumite, Kata, tanto en masculino como en femenino para los próximos Juegos Deportivos Nacionales, Guanacaste 2024."
+                    },
+                    {
+                        subtitle: "Felicitaciones",
+                        paragraph: 
+`Felicitamos a todos los atletas de Karate Belén por garantizar su
+presencia y competencia con la bandera de Belén para estas justas
+guanacastecas.🤩
+
+Belén, Ciudad Americana del Deporte 2024
+3 de junio
+                    `
+                    },
+                ]
+            },
+            {
+                title: "Copa Ryūko Kai",
+                image: "./assets/blog/articles/article1/2.png",
+                description: [
+                    {
+                        subtitle: "Copa Ryūko Kai",
+                        paragraph: "Este domingo anterior, 15 de nuestros atletas de Karate Do Belén compitieron en el Torneo Ryuko kai."
+                    },
+                    {
+                        subtitle: "Resultados:",
+                        paragraph: 
+`🔹 Mathias Portillo: 1er lugar Kata
+🔹 Brittany Rodríguez: 1er lugar Kata y 1er lugar Kumite
+🔹 Tomás Quesada: 1er lugar Kata y 1er lugar kumite
+🔹 Luis Viquez: 1er lugar Kata y 2do lugar kumite
+🔹 Sofía Viquez: 2do lugar kata y 1er lugar kumite
+🔹 Sol Lehouiller: 2do lugar kata
+🔹 Ailyn Quesada: 2do lugar Kata y 3er Lugar kumite
+🔹 Mónica Portillo: 2do lugar kata y 2do lugar kumite
+🔹 Cristina Portillo: 2do lugar Kata
+🔹 Emiliano Ramírez: 3er lugar kata y 3er Lugar Kumite
+🔹 Santiago Garcia: 3er lugar kata y 3er Lugar kumite
+🔹 Juan Pablo Lamuraglia: 3er lugar Kumite
+🔹 Emiliano Pachame: 3er lugar Kumite
+🔹 Danna Martínez: 3er lugar Kumite
+🔹 Samira Bustamante: Excelente participación
+                    `
+                    },
+                    {
+                        subtitle: "Medallas:",
+                        paragraph: 
+`Para un total de 🥇 7 medallas de Oro, 🥈 7 medallas de plata y 🥉 8 medallas de Bronce.
+
+Felicitamos a todos los atletas que estuvieron dando su mejor esfuerzo en este torneo y que llevan nuestros colores en cada rincón de nuestro país con gran entusiasmo y honor.
+
+Belén, Ciudad Americana del Deporte 2024
+21 de mayo
+                    `
+                    },
+                ]
+            },
+            {
+                title: "Ranking Nacional FEKOKA",
+                image: "./assets/blog/articles/article1/3.png",
+                description: [
+                    {
+                        subtitle: "Ranking Nacional",
+                        paragraph: "Este fin de semana nuestro Dojo Fudoshin, estuvo presente en el Campeonato Infantil FECOKA y Ranking Nacional, Serie CRC. 🇨🇷"
+                    },
+                    {
+                        subtitle: "Resultados:",
+                        paragraph: 
+`
+Tomás Quesada: 🥇Kata y 🥈 kumite 
+Ailyn Quesada: 🥉 Kata y 🥈 kumite
+Rebeca López: 🥉 Kata 
+Yeilin Monge: 🥉 Kata 🥉
+Nefher Menocal: 🥈Kumite 
+María Fernanda Mendoza: 🥇 Kumite 
+                    
+Felicitamos a todos los atletas del Dojo por presentar nuestros valores en cada torneo y campeonato en los que se presentan, a cada uno de los padres, atletas y a Sensei Sergio Cambronero por su gran trabajo con los atletas.🥋.
+                    
+Belén, Ciudad Americana del Deporte 2024
+29 de abril
+                    `
+                    },
+                ]
+            },
+        ]
+    },
+];
---
+++ js/blog.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const blogPostsContainer = document.getElementById('blog-posts');
+
+    if (blogPostsContainer) {
+        loadBlogPosts();
+    } else {
+        const articleId = new URLSearchParams(window.location.search).get('id');
+        if (articleId) {
+            loadArticle(articleId);
+        }
+    }
+});
+
+function loadBlogPosts() {
+    const blogPostsContainer = document.getElementById('blog-posts');
+    articles.forEach(article => {
+        const articleLink = document.createElement('a');
+        articleLink.href = `article.html?id=${article.id}`;
+
+        const articleElement = document.createElement('div');
+        articleElement.classList.add('post');
+
+        articleElement.innerHTML = `
+            <img src="${article.thumbnail}" alt="Imagen de ${article.title}">
+            <h2>${article.title}</h2>
+            <p>${article.content[0].description.substring(0, 100)}...</p>
+        `;
+
+        articleLink.appendChild(articleElement);
+        blogPostsContainer.appendChild(articleLink);
+    });
+}
+
+function loadArticle(id) {
+    const article = articles.find(a => a.id == id);
+    if (article) {
+        document.getElementById('article-title').innerText = article.title;
+        const articleContentContainer = document.getElementById('article-content');
+        article.content.forEach(contentArticle => {
+            const div = document.createElement('div');
+            div.classList.add('contentArticle');
+            const h2 = document.createElement('h2');
+            const img = document.createElement('img');
+            h2.textContent = contentArticle.title;
+            img.src = contentArticle.image;
+            div.appendChild(h2);
+            div.appendChild(img);
+
+            contentArticle.description.forEach(descripciones => {
+                const h3 = document.createElement('h3');
+                const p = document.createElement('p');
+                h3.textContent = descripciones.subtitle;
+                p.textContent = descripciones.paragraph;
+                div.appendChild(h3);
+                div.appendChild(p);
+            });
+            
+            articleContentContainer.appendChild(div);
+            
+        });
+    } else {
+        document.getElementById('article-title').innerText = "Artículo no encontrado";
+    }
+}
